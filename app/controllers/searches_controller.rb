e093f6fe9121f8d6f41e5018516074bafebfcce1
dlee16 <diana.lee116@gmail.com>
2019-03-25 12:19:57 -0400

    updates to search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,19 @@
 class SearchesController < ApplicationController
   def new
     @search = Search.new
+    @flights = Flight.select('DISTINCT state')
+    @returnflight = Flight.select('DISTINCT return_state')
     render :new
   end
 
   def create
-    @search = Search.create!(search_params)
-    redirect_to search_path(@search.id)
+    @search = Search.create(search_params)
+    if @search.valid?
+      redirect_to search_path(@search.id)
+    else
+      @errors = @search.errors.full_messages
+      render :new
+    end
   end
 
   def show
